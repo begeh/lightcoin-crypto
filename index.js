27d3665aaa6aaa07338921b1eb3dd15083fc5922
Bashir Egeh <b_egeh@hotmail.com>
2020-01-17 21:05:10 +0000

    first commit

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ class Withdrawal {
   commit() {
     balance -= this.amount;
   }
-
 }
 
+class Deposit {
+  constructor(amount) {
+    this.amount = amount;
+  }
 
+  commit() {
+    balance += this.amount;
+  }
+}
 
+class Account{
+  constructor(username){
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
@@ -26,4 +39,9 @@ t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
+t3 = new Deposit(120.00);
+t3.commit();
+console.log('Transaction 3: ', t3);
+
 console.log('Balance:', balance);
+
